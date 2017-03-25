59eb9352911466b6e2938f81ce6f99ee72d1b753
Sabrina Ip <kkip90@gmail.com>
2017-03-24 20:50:23 -0400

    create storyboard

---
+++ SpotifyClient/SpotifyClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyClient
+//
+//  Created by Sabrina Ip on 3/24/17.
+//  Copyright © 2017 Sabrina Ip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
